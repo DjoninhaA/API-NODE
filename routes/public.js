9bbcae25f5b77b442be8f29219ba65339283c0a5
djoninhaA <djonathan8016@gmail.com>
2025-03-10 09:44:41 -0300

    dependencies:JWT

--- routes/public.js
+++ routes/public.js
@@ -1,22 +1,31 @@
-import express from 'express'
-import { PrismaClient } from '@prisma/client'
+import express from "express";
+import bcrypt from "bcrypt";
+
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+router.post("/cadastro", async (req, res) => {
+  try {
+    const user = req.body;
+
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(user.password, salt);
+
+    const userDb = await prisma.user.create({
+      data: {
+        email: user.email,
+        name: user.name,
+        password: hashPassword,
+      },
+    });
+    res.status(201).json(userDb);
+  } catch (err) {
+    res.status(500).json({ message: "Erro no Servidor." });
+  }
+});
 
 
-const prisma = new PrismaClient()
-const router = express.Router()
 
-//cadastro
-router.post('/cadastro', async (req,res) => {
-    const user = req.body
-
-    await prisma.user.create({
-        data:{
-            email: user.email,
-            name: user.name,
-            password
-        }
-    })
-    res.status(201).json(user)
-})
-
-export default router
+export default router;
