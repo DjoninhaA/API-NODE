4b05804bd5eb8d4d6ffe7051f4ecba36dcf1d308
djoninhaA <djonathan8016@gmail.com>
2025-03-10 09:11:08 -0300

    changing code

--- server.js
+++ server.js
@@ -9,10 +9,3 @@ app.use("/", publicRoutes);
 app.listen(3000, () => {
   console.log("Server On...");
 });
-
-
-// name="djonathan"
-
-// key="xpaqptDlJKRAQ8cr"
-
-// url="mongodb://djonathan:<db_password>@<hostname>/?ssl=true&replicaSet=atlas-p02fqp-shard-0&authSource=admin&retryWrites=true&w=majority&appName=users"
